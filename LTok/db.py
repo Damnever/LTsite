8d114744e834e7261e17b22f90182c5a58751cb3
Last_D <dxc_wolf@163.com>
2014-10-26 20:05:19 +0800

    reference ORM

--- LTok/db.py
+++ LTok/db.py
@@ -5,7 +5,7 @@
 --------------------------------------------------------------------------------
     Author: Last_D
     Created Time: 2014-10-25 11:10:37 Sat
-    Last Modified: 2014-10-25 21:38:05 Sat
+    Last Modified: 2014-10-26 20:04:04 Sun
     Description:
         A module for operating database, include a ORM framework.
         -> https://github.com/michaelliao/transwarp/blob/master/transwarp/db.py
@@ -22,6 +22,9 @@ class InterfaceError(Exception):
     pass
 
 class DatabaseError(Exception):
+    pass
+
+class ProgrammingError(DatabaseError):
     pass
 
 class MultiColumnError(DatabaseError):
@@ -258,9 +261,9 @@ def update_kw(table, conditions, *args, **kwargs):
 
     Parameters:
         table: table name.
-        conditions: conditions, such as ['id=? or id=?'], which can be None.
-        args: args for conditions, such as [1, 2].
-        kwargs: column name and value pairs, such as [name='Last_D']
+        conditions: conditions, such as `'id=? or id=?'`, which can be None.
+        args: args for conditions, such as `1, 2`.
+        kwargs: column name and value pairs, such as `name='Last_D'`
     """
     if len(kwargs) == 0:
         raise ValueError('No keyword args.')
@@ -277,6 +280,17 @@ def update_kw(table, conditions, *args, **kwargs):
     params.extend(args)
     return _update(sql, *params)
 
+@with_connection
+def delete(table, conditions, *args):
+    """Execute delete SQL.
+    Parameters:
+        table: table name.
+        conditions: such as `id=? and name=?`.
+        args: arguments for conditions, such as `1, 'Last_D'`.
+    """
+    sql = 'delete from %s where %s' % (table, conditions)
+    return _update(sql, *args)
+
 
 class Field(object):
 
@@ -291,5 +305,229 @@ class Field(object):
         self.insertable = kwargs.get('insertable', True)
         self.ddl = kwargs.get('ddl', '')
 
-        self._order = Field._count
+        self.order = Field._count
         Field._count += 1
+
+    @property
+    def default(self):
+        d = self._default
+        return d() if callable(d) else d
+
+class StringField(Field):
+
+    def __init__(self, **kwargs):
+        if not 'default' in kwargs:
+            kwargs['default'] = ''
+        if not 'ddl' in kwargs:
+            kwargs['ddl'] = 'varchar(255)'
+        super(StringField, self).__init__(**kwargs)
+
+class TextField(Field):
+
+    def __init__(self, **kwargs):
+        if not 'default' in kwargs:
+            kwargs['default'] = ''
+        if not 'ddl' in kwargs:
+            kwargs['ddl'] = 'text'
+        super(TextField, self).__init__(**kwargs)
+
+class IntegerField(Field):
+
+    def __init__(self, **kwargs):
+        if not 'default' in kwargs:
+            kwargs['default'] = 0
+        if not 'ddl' in kwargs:
+            kwargs['ddl'] = 'int'
+        super(IntegerField, self).__init__(**kwargs)
+
+class FloatField(Field):
+
+    def __init__(self, **kwargs):
+        if not 'default' in kwargs:
+            kwargs['default'] = 0.0
+        if not 'ddl' in kwargs:
+            # if not real as float, real will be double precision.
+            kwargs['ddl'] = 'real'
+        super(FloatField, self).__init__(**kwargs)
+
+class BooleanField(Field):
+
+    def __init__(self, **kwargs):
+        if not 'default' in kwargs:
+            kwargs['default'] = False
+        if not 'ddl' in kwargs:
+            kwargs['ddl'] = 'bool'
+        super(BooleanField, self).__init__(**kwargs)
+
+class BlobField(Field):
+
+    def __init__(self, **kwargs):
+        if not 'default' in kwargs:
+            kwargs['default'] = ''
+        if not 'default' in kwargs:
+            kwargs['ddl'] = 'blob' # 64k
+        super(BlobField, self).__init__(**kwargs)
+
+
+def _gen_sql(table, mappings):
+    """Generate SQL statement.
+    Such as:
+        create table users (
+            `id` int not null.
+            `name` varchar(25) not null,
+            primary_key (`id`)
+        );
+    """
+    primary_key = None
+    sql = ['create table %s (' % table]
+    for f in sorted(mappings.itervalues(), lambda x,y: cmp(x.order,y.order)):
+        if not hasattr(f, 'ddl'):
+            raise ProgrammingError("No ddl in field '%s'" % f.name)
+        if f.primary_key:
+            primary_key = f.name
+        sql.append(f.nullable and '    %s %s,' % (f.name, f.ddl) or \
+                '    %s %s not null,' (f.name, f.ddl))
+    sql.append('primary key (%s)' % primary_key)
+    sql.append(');')
+    return '\n'.join(sql)
+
+class ModelMetaClass(type):
+    """
+    Metaclass for model.
+    """
+    def __new__(cls, name, bases, attrs):
+        # skip base Model class
+        if name == 'Model':
+            return type.__new__(cls, name, bases, attrs)
+
+        # store all subclass info
+        if not hasattr(cls, 'subclasses'):
+            cls.subclasses = {}
+        if not name in cls.subclasses:
+            cls.subclasses[name] = name
+        else:
+            logging.warning('Redefine class: %s' % name)
+
+        logging.info('Scan Object Relational Mapping %s ...' % name)
+        mappings = dict()
+        primary_key = None
+        for k, v in attrs.iteritems():
+            if isinstance(v ,Field):
+                if not v.name:
+                    v.name = k
+                logging.info('Found mappings: %s => %s' % (k, v))
+                # check duplicate primary key
+                if v.primary_key:
+                    if primary_key:
+                        raise ProgrammingError("Cannot defined more than one primary \
+                                key in class: '%s'." % name)
+                    if v.updateable:
+                        logging.warning('Note: change primary key to non-updateable.')
+                        v.updateable = False
+                    if v.nullable:
+                        logging.warning('Note: change primary key to non-nullable.')
+                        v.nullable = False
+                    primary_key = v.name
+                mappings[k] = v
+        # check exists of primary key
+        if not primary_key:
+            raise ProgrammingError("Primary key not in class: '%s'" % name)
+        for k in attrs:
+            attrs.pop(k)
+        attrs['__table__'] = name.lower()
+        attrs['__mappings__'] = mappings
+        attrs['__primary_key__'] = primary_key
+        def _sql(self):
+            return _gen_sql(name.lower(), mappings)
+        attrs['__sql__'] = _sql
+        return type.__new__(cls, name, bases, attrs)
+
+class Model(dict):
+    """
+    Base class for ORM.
+
+    Example:
+        class User(Model):
+            id = IntegerField(primary_key=True)
+            name = StringField()
+            email = StringField()
+    """
+    __metaclass__ = ModelMetaClass
+
+    def __init__(self, **kwargs):
+        super(Model, self).__init__(**kwargs)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    @classmethod
+    def get_by_primary_key(cls, primary_key):
+        """Execute select SQL by primary key."""
+        d = select_one('select * from {0} where {1}=?'.format(cls.__table__, \
+                cls.__primary_key__.name), primary_key)
+        return cls(**d) if d else None
+
+    @classmethod
+    def select_one(cls, conditions, *args):
+        """
+        Execute select SQL and return first one result.
+
+        Parameters:
+            conditions: such as `id=?` or None.
+            argss: args for conditions, such as `1` or nothing.
+        """
+        if conditions:
+            d = select_one('select * from {0} where {1}'.format(cls.__table__, \
+                    conditions), *args)
+        else:
+            d = select_one('select * from %s' % cls.__table)
+        return cls(**d) if d else None
+
+    @classmethod
+    def count(cls, conditions, *args):
+        """Count rows of the table by conditions or not, then return a
+        number. Parameters `args` for conditions if exists.
+        """
+        if conditions:
+            num = select_int('select count({0}) from {1} where {2}'.format(\
+                cls.__primary__key__.name, cls.__table__, conditions), *args)
+        else:
+            num = select_int('select count({0}) from {1}'.format(\
+                    cls.__primary_key__.name, cls.__table__))
+        return num
+
+    def update(self):
+        kw = {}
+        for k, v in self.__mappings__.iteritems():
+            if v.updateable:
+                if hasattr(self, k):
+                    arg = getattr(self, k)
+                else:
+                    arg = v.default
+                    setattr(self, k, arg)
+                kw[k] = arg
+        pk = self.__primary_key__.name
+        update_kw(self.__table__, '%s=?'%pk, getattr(self, pk), **kw)
+        return self
+
+    def delete(self):
+        pk = self.__primary_key__.name
+        args = getattr(self, pk)
+        _update('delete from {0} where {1}=?'.format(self.__table__, pk), args)
+        return self
+
+    def insert(self):
+        params = {}
+        for k, v in self.__mappings__.iteritems():
+            if k.insertable:
+                if not hasattr(self, k):
+                    setattr(self, k, v.default)
+                params[v.name] = getattr(self, k)
+        insert(self.__table__, **params)
+        return self
